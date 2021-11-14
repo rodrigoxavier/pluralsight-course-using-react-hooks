57788d7dad50812843161080bd96f5ca4db8309a
Rodrigo Xavier <rodrigo.henrique@take.net>
2021-11-14 16:45:55 -0300

    Setting up project and using useState

--- 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
+++ 02-Basic-React-Hooks-useState-useRef-useEffect/clip2-Setting-Up-Toolchain/pages/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
+
 
 const InputElement = () => {
-  return <input placeholder="Enter Some Text" />;
+  
+  const [inputText, setInputText] = useState("");
+  const [historyList, setHistoryList] = useState([])
+
+  return (
+    <div>
+      <input onChange={(e) => {
+        setInputText(e.target.value);
+        setHistoryList([...historyList, e.target.value]);
+      }} placeholder="Enter Some Text" value={inputText} />
+      <br/>
+      {inputText}
+      <hr/><br/>
+      <ul>
+        {historyList.map((record) => {
+          return <div>{record}</div>
+        })}
+      </ul>
+    </div>
+  )
 };
 
 export default InputElement;
